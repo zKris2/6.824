cdb075aa556c5156b559e63e77e3e62c43933ba9
hyler <2813446928@qq.com>
2024-06-07 17:33:07 +0800

    Success

--- src/master.cpp
+++ src/master.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
+#include <fstream>
 #include "buttonrpc/buttonrpc.hpp"
 
 #define MAP_TASK_TIMEOUT 3
@@ -17,28 +18,32 @@ class Master{
 		bool isMapDone();
 		Master(int mapNum = 8,int reduceNum = 8);//构造函数
 		void GetAllFiles(char* file[],int argc);//从args[]里获取待处理的文件名
-		
+		void Split(const std::string& file);
+
 		int getMapNum(){return m_maps.size();}
 		int getReduceNum(){return m_reduces.size();}
 
-		std::string assignTask();//分配map任务，RPC
+		std::string assignMapTask(std::string workerID);
+		std::string assignReduceTask(std::string workerID);
+
                 void waitTime(std::string type);
 		void waitMap(char* file);
                 void checkMapTask(char* file);
 
 		void setMapState(std::string file,bool state);
+		void setReduceState(std::string file,bool state);
 	private:
 		std::mutex m_mtx;
 		bool m_done;
 		int m_fileNum;//命令行文件数
 
-		std::queue<char *> m_maps;//所有map任务的工作队列
+		std::queue<std::string> m_maps;//所有map任务的工作队列
 		int m_mapNum;
 		std::vector<std::string> m_runningMaps;//正在处理的map任务，分配出去就加入这个队列，用于判断超超时处理重发
 		int m_curMapIndex;//当前正在处理第几个Map任务
 		std::unordered_map<std::string,int> m_finishedMaps;//存放已完成的map任务对应的文件名
 
-		std::queue<int> m_reduces;//所有reduce任务工作队列
+		std::queue<std::string> m_reduces;//所有reduce任务工作队列
 		int m_reduceNum;		
 		std::vector<std::string> m_runningReduces;//正在处理的reduce任务，分配出去就加入这个队列，用于判断超超时处理重发
 		int m_curReduceIndex;//当前正在处理第几个Reduce任务		
@@ -47,8 +52,7 @@ class Master{
 
 bool Master::isMapDone(){
 	std::lock_guard<std::mutex> lk(m_mtx);
-       	std::cout<< "已完成任务数量 : " << m_finishedMaps.size() <<std::endl; 
-	if(m_finishedMaps.size()!=m_fileNum){
+	if(m_finishedMaps.size() < static_cast<size_t>(m_fileNum)){
                 return false;
         }
         return true;
@@ -68,11 +72,48 @@ Master::Master(int mapNum,int reduceNum):m_done(false),m_mapNum(mapNum),m_reduce
 	}
 };
 
+//切分大小定义
+#define SPLIT_SIZE 1024*64 //64kb
+// 分块编号
+int split_num = 1;
+//文件切分
+void Master::Split(const std::string& file){
+	std::ifstream ifs(file);
+	if (!ifs.is_open()){
+		std::cerr << "文件切分出错" << std::endl;
+		exit(0);
+	}
+	char* buffer = new char[SPLIT_SIZE];
+	while(!ifs.eof()){
+		ifs.read(buffer,SPLIT_SIZE);
+		std::streamsize read_size = ifs.gcount();
+
+		if(read_size > 0){
+			std::string split_file_name = "split-" + std::to_string(split_num);
+                    	std::ofstream ofs(split_file_name);
+			
+			if(!ofs.is_open()){
+				std::cerr << "打开输出文件 : " << split_file_name << " 失败" << std::endl;
+                        	continue;	
+			}
+			// 将分块数据写入新文件
+                   	ofs.write(buffer, read_size);
+                    	ofs.close();
+
+                    	std::cout << "成功创建分块文件 : " << split_file_name << std::endl;
+                    	//加入map任务队列
+			m_maps.push(split_file_name);
+			split_num++;
+		}
+	}
+}
 
 void Master::GetAllFiles(char* file[],int argc){
 	for(int i=1; i<argc; i++){
-		m_maps.push(file[i]);
-		std::cout << "argv[" << i << "] : " << file[i] <<std::endl; 
+		//m_maps.push(file[i]);
+		std::cerr << "argv[" << i << "] : " << file[i] <<std::endl; 
+		//文件分块
+		Split(std::string(file[i]));
 	}	
 	m_fileNum = argc-1;	
 };
@@ -107,17 +148,28 @@ void Master::checkMapTask(char* file){
 	std::cout << "task file[" << file <<"] is finished!" << std::endl;
 }
 
-std::string Master::assignTask(){
+std::string Master::assignMapTask(std::string workerID){
 	std::lock_guard<std::mutex> lk(m_mtx);
-        std::cout << "未完成任务数量 : " << m_maps.size()<<std::endl;
-        //将工作队列任务放到 正在运行的map任务队列
         if(!m_maps.empty()){
-                char* file = m_maps.front();//从工作队列取出一个待map的文件名
-                std::cout << "  	分配文件["<< file <<"]到worker" << std::endl;
+                std::string file = m_maps.front();
+                std::cout << "  	分配map文件 ["<< file <<"] 到 worker[" << workerID << "]" << std::endl;
                 m_maps.pop();
 		// waitMap(file);
-		m_runningMaps.emplace_back(std::string(file));
+		m_runningMaps.emplace_back(file);
                 return std::string(file);
+        }
+        return "empty";
+};
+
+std::string Master::assignReduceTask(std::string workerID){
+	std::lock_guard<std::mutex> lk(m_mtx);
+        if(!m_reduces.empty()){
+                std::string file = m_reduces.front();
+                std::cout << "  	分配reduce文件 ["<< file <<"] 到 worker[" << workerID << "]" << std::endl;
+                m_reduces.pop();
+		// waitMap(file);
+		m_runningReduces.emplace_back(file);
+                return file;
         }
         return "empty";
 };
@@ -127,6 +179,7 @@ void Master::setMapState(string file,bool state){
 	if(state){
 		m_finishedMaps[file] = 1;
 		std::cout << "文件[" << file <<"]处理完成!" << std::endl;
+		m_reduces.push(file);
 	} else {
 		//未完成
                 //重新加入map任务队列
@@ -136,17 +189,29 @@ void Master::setMapState(string file,bool state){
                 std::cout << "文件处理出错，重新添加文件[" << file << "]到任务队列" << std::endl;
 	} 
 }
-
+void Master::setReduceState(string file,bool state){
+	if(state){
+		m_finishedMaps[file] = 1;
+		std::cout << "文件[" << file <<"]处理完成!" << std::endl;
+		//m_finishedReduces.push(file);
+	} else {
+		//未完成
+                //重新加入map任务队列	
+                m_reduces.push(file);
+                std::cout << "文件处理出错，重新添加文件[" << file << "]到任务队列" << std::endl;
+	} 
+}
 int main(int argc,char* argv[]) {
         buttonrpc s;
         s.as_server(8080);
-
         Master master(13,9);
         master.GetAllFiles(argv,argc);
         s.bind("getMapNum",&Master::getMapNum,&master);
         s.bind("getReduceNum",&Master::getReduceNum,&master);
-        s.bind("assignTask",&Master::assignTask,&master);
+        s.bind("assignMapTask",&Master::assignMapTask,&master);
+        s.bind("assignReduceTask",&Master::assignReduceTask,&master);
 	s.bind("setMapState",&Master::setMapState,&master);
+	s.bind("setReduceState",&Master::setReduceState,&master);
 
         s.run();
         return 0;
--- src/worker.cpp
+++ src/worker.cpp
@@ -11,163 +11,122 @@
 #include <queue>
 #include <mutex>
 
+#define RPC_IP "127.0.0.1"
+#define RPC_PORT 8080
 
-//分块文件
-std::queue<std::string> splits;
-// 分块大小，可以根据需要调整
-const int SPLIT_SIZE = 1024*64; // 例如，64KB
-// 分块编号
-int split_num = 1;
-
-//文件分割
-void split(){
-
-        buttonrpc client;
-        client.as_client("127.0.0.1",8080);
-
-	while(1){
-		std::string file = client.call<std::string>("assignTask").val();	
-		if (file == "empty"){
-                        std::cerr << "map队列为空" << std::endl;
-			return;
-		}
-		std::ifstream ifs(file);
-		if(!ifs.is_open()){
-			std::cerr << "无法打开输入文件：" << file << std::endl;
-			client.call<void>("setMapState",file,false);//重新加入任务队列
-		}
-		// 缓冲区
-		char* buffer = new char[SPLIT_SIZE];
-		// 读取并分块文件
-		while (!ifs.eof()) {
-			ifs.read(buffer, SPLIT_SIZE);
-			std::streamsize read_size = ifs.gcount();
-
-			if (read_size > 0) {
-			    std::string output_file_name = "split_" + std::to_string(split_num) + ".txt";
-			    std::ofstream output_file(output_file_name);
-			    
-			    if (!output_file.is_open()) {
-				std::cerr << "无法创建输出文件" << std::endl;
-			    }
-			    
-			    // 将分块数据写入新文件
-			    output_file.write(buffer, read_size);
-			    output_file.close();
-			    
-			    std::cout << "已创建分块文件 : " << output_file_name << std::endl;
-			    split_num++;
-			    splits.push(output_file_name);
-			}
-		}
-
-		delete[] buffer;
-		ifs.close();	
-		client.call<void>("setMapState",file,true);				
-	}
-}
-
-
-// map中间文件编号
-int mr_num = 1;
-
-bool writeToDisk(std::map<std::string,int>& interMap , string file){
-	
-	std::string mrfile = "mr_" + file;
-	std::ofstream ofs(mrfile);
-	//------------------- 测试map失败情况（master重新加入map任务队列）-----------------
-	//if(outNum==1){
-	//	std::cout << "map inter error" << std::endl;
-	//	outNum++;
-	//	//std::this_thread::sleep_for(std::chrono::seconds(3));
-	//	 // 尝试删除文件
-	//	if (std::remove(outfile.c_str()) == 0) {
-	//		std::cout << "delete file success!" << std::endl;
-	//	} else {
-	//		std::cout << "delete file error!" << std::endl;
-	//	}
-	//	std::cout << "close ofs"<<std::endl;
-	//	ofs.close();
-	//	return false;
-	//}
-	//------------------- 测试map失败情况（master重新加入map任务队列）-----------------
-	
+bool Write2Disk(std::map<std::string,int>& map, const string& file){
+	std::ofstream ofs(file);
 	if(!ofs.is_open()){
+		std::cerr << "输出文件[" << file << "]打开失败" << std::endl;
 		return false;		
 	}
-	for(auto& pair : interMap){
+	for(const auto& pair : map){
 		ofs << pair.first << " : " <<pair.second << std::endl; 
         }
 	ofs.close();
-	mr_num++;
 	return true;
 }
+void Del4Disk(const std::string& file){
+		
+}
 
-//锁
-std::mutex mtx;
 //中间结果
-std::map<std::string,int> interMap;
+std::map<std::string,std::vector<int>> inter_map;
+
+bool MapHelper(std::string file,std::map<std::string,int>& output){
+	std::ifstream ifs(file);
+	if(!ifs.is_open()){
+		std::cerr << "打开文件-" << file << "-失败" << std::endl;
+		return false;
+	}
+	std::string line;
+	while(std::getline(ifs,line)){
+		std::istringstream iss(line);
+		std::string word;
+		while(iss >> word){
+			output[word]++;
+		}
+	}
+	ifs.close();
+	return true;	
+}
 
 void Map(){
 	buttonrpc client;
 	client.as_client("127.0.0.1",8080);
-	while(1){
-		mtx.lock();
-                if(splits.size() <= 0){
-			std::cerr << "split 文件处理完成" << std::endl;
-			return;
-		}
-                std::string file = splits.front();
-		splits.pop();
-		mtx.unlock();//解锁
-
-		std::cerr << "获取split文件 : " << file << std::endl;
-	        std::ifstream ifs(file);
-        	if(!ifs.is_open()){
-                	std::cerr << file << "打开失败!" <<std::endl;
-			splits.push(file);
-			continue;
-        	}
-		//读取文件内容
-		std::string line;
-		while(std::getline(ifs,line)){
-			std::istringstream iss(line);
-			std::string word;
-			while(iss >> word){
-				interMap[word]++;
-			}
-		}
 	
-		//生成中间文件
-		if(!writeToDisk(interMap,file)){
-			client.call<void>("setMapState",file,false);
-			continue;	
+	while(1){
+		std::string file = client.call<std::string>("assignMapTask","001").val();
+		if (file == "empty"){
+                        return;
 		}
-		
-		//通知master 当前map文件完成
-		client.call<void>("setMapState",file,true);
-		std::cerr << "文件[" << file << "]处理完成！" << std::endl;
+		std::cerr << "获取文件 : " << file << std::endl;
+		std::map<std::string,int> output;
+		if(!MapHelper(file,output)){
+			std::cerr << file << "Map 失败" << std::endl;	
+			client.call<void>("setMapState",file,false);
+		}else{
+			Write2Disk(output,"map-"+file);
+			client.call<void>("setMapState","map-"+file,true);
+		}
         }
 }
 
-void reduce(const std::string& word,const std::vector<int>& counts,std::map<std::string,int>& output){
-	
+void Shuffle(){
+	buttonrpc client;
+        client.as_client("127.0.0.1",8080);
+	while(1){
+		std::string file = client.call<std::string>("assignReduceTask","001").val();
+		if(file=="empty"){
+			return;
+		}
+		std::ifstream ifs(file);
+		if(!ifs.is_open()){
+			std::cerr <<"reduce-"<< file << "-打开失败" << std::endl;
+			client.call<void>("setReduceState",file,false);
+			continue;
+		}
+		std::string line;
+		while(std::getline(ifs,line)){
+			std::istringstream iss(line);
+			std::string key;
+			int value;
+			 // 读取键
+			if (getline(iss, key, ':')) {
+			    // 跳过空格
+			    iss >> ws;
+			    // 读取值
+			    if (iss >> value) {
+				// 将键值对插入map中，如果键已存在，则会更新其值
+				inter_map[key].emplace_back(value);
+			    }
+			}
+		}
+		ifs.close();
+	}
+}
+void Reduce(){
+	std::map<std::string,int> output;
+	for (auto it = inter_map.begin();it!=inter_map.end();it++){
+		int sum = 0;
+		for(int i=0;i<(*it).second.size();i++){
+			sum += (*it).second[i];
+		}
+		output[(*it).first] = sum;
+	}
+	Write2Disk(output,"out-put");
 }
 
-
-
 int main(){
-
-        buttonrpc client;
+        
+	buttonrpc client;
         client.as_client("127.0.0.1",8080);
         client.set_timeout(5000);
         int ret = client.call<int>("getMapNum").val();
         int ret1 = client.call<int>("getReduceNum").val();
         std::cout << "mapNum:" << ret << " reduceNum:" << ret1 << std::endl;
 
-	split();
-
 	Map();
-	
-	//Reduce();
+	Shuffle();
+	Reduce();
 };
